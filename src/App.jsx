af56479465b57c3a66a4d93adef9b0678ace7081
casinocasino <182699034+casinocasino@users.noreply.github.com>
2024-10-26 05:44:00 +1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ import {
   FormControl,
   InputLabel,
   Select,
-  MenuItem
+  MenuItem,
+  Slider
 } from '@mui/material';
 import './App.css';
 
@@ -35,22 +36,21 @@ const rows = [];
 for (let i = 1; i <= 12; i++) {
   rows.push({
     lives: i,
-    value: 2 ** i - 1,
-    lossChance: ((1 / 2) ** i * 100).toFixed(3)
+    value: 2 ** i - 1
   });
 }
 
 function App() {
   const [bankroll, setBankroll] = useState(90);
   const [lives, setLives] = useState(2);
-  const [wins, setWins] = useState(0);
   const [increment, setIncrement] = useState(1);
+  const [winOdds, setWinOdds] = useState(18 / 37);
 
-  const optimalBet = () => {
+  const optimalBet = (n) => {
     if (!isNumber(bankroll) || !isNumber(lives)) {
       return 'N/A';
     } else {
-      const bet = parseFloat(bankroll) / (2 ** parseFloat(lives) - 1);
+      const bet = parseFloat(bankroll) / (2 ** parseFloat(n) - 1);
       return Math.floor(bet / increment) * increment;
     }
   };
@@ -62,19 +62,15 @@ function App() {
         <Typography variant="h2" component="h2" marginBottom={2}>
           Calculator
         </Typography>
-        <Typography variant="body1">Optimal Bet: {optimalBet()}</Typography>
-        <Typography variant="body1" marginBottom={2}>
-          Number of Wins: {wins}
-        </Typography>
-        <div>
-          <Stack direction="column" spacing={2} marginBottom={2}>
+        <Stack direction="column" spacing={2} marginBottom={2}>
+          <Stack direction="row" spacing={2}>
             <TextField
+              sx={{ width: '100%' }}
               id="bankroll"
               label="Bankroll"
               variant="outlined"
               onChange={(event) => {
                 setBankroll(event.target.value);
-                setWins(0);
               }}
               error={!isNumber(bankroll)}
               helperText={!isNumber(bankroll) ? 'Please enter a number' : ''}
@@ -87,19 +83,7 @@ function App() {
                 }
               }}
             />
-            <TextField
-              id="lives"
-              label="Lives"
-              variant="outlined"
-              onChange={(event) => {
-                setLives(event.target.value);
-                setWins(0);
-              }}
-              error={!isNumber(lives)}
-              helperText={!isNumber(lives) ? 'Please enter a number' : ''}
-              value={lives}
-            />
-            <FormControl fullWidth>
+            <FormControl fullWidth sx={{ width: '100%' }}>
               <InputLabel id="increment-label">Rounding Increment</InputLabel>
               <Select
                 labelId="increment-label"
@@ -118,46 +102,100 @@ function App() {
                 <MenuItem value={100}>100</MenuItem>
               </Select>
             </FormControl>
-            <Stack direction="row" spacing={2} justifyContent="end">
-              <Button
-                variant="contained"
-                id="calculate"
-                onClick={() => {
-                  if (!isNumber(bankroll) || !isNumber(lives)) {
-                    return;
-                  }
-                  setBankroll(parseFloat(bankroll) + optimalBet());
-                  setWins(wins + 1);
-                }}
-              >
-                Next Bet
-              </Button>
-            </Stack>
           </Stack>
-        </div>
+          <Stack direction="row" spacing={4}>
+            <Typography id="lives-label" align="left">
+              Lives
+            </Typography>
+            <Slider
+              id="lives-slider"
+              label="Lives"
+              value={lives}
+              onChange={(_event, value) => {
+                setLives(value);
+              }}
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={1}
+              max={12}
+              aria-labelledby="lives-label"
+            />
+          </Stack>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Lives</TableCell>
+                  <TableCell align="right">Optimal Bet</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Array.from({ length: 10 }, (_, i) => i + 1).map((n) => (
+                  <TableRow key={n}>
+                    <TableCell>{n}</TableCell>
+                    <TableCell align="right">{optimalBet(n)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Stack direction="row" spacing={2} justifyContent="end">
+            <Button
+              variant="contained"
+              id="calculate"
+              onClick={() => {
+                if (!isNumber(bankroll) || !isNumber(lives)) {
+                  return;
+                }
+                setBankroll(parseFloat(bankroll) + optimalBet(lives));
+              }}
+            >
+              Next Bet
+            </Button>
+          </Stack>
+        </Stack>
         <Typography variant="h2" component="h2" marginBottom={2}>
           Values
         </Typography>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Lives</TableCell>
-                <TableCell align="right">Bankroll Divisor</TableCell>
-                <TableCell align="right">Chance of Loss</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow key={row.lives}>
-                  <TableCell>{row.lives}</TableCell>
-                  <TableCell align="right">{row.value}</TableCell>
-                  <TableCell align="right">{row.lossChance + '%'}</TableCell>
+        <Stack direction="column" spacing={2}>
+          <FormControl fullWidth sx={{ width: '100%' }}>
+            <InputLabel id="win-odds-label">Win Odds</InputLabel>
+            <Select
+              labelId="win-odds-label"
+              id="win-odds"
+              value={winOdds}
+              label="Win Odds"
+              onChange={(event) => setWinOdds(event.target.value)}
+              align="left"
+            >
+              <MenuItem value={18 / 37}>18/37</MenuItem>
+              <MenuItem value={18 / 38}>18/38</MenuItem>
+            </Select>
+          </FormControl>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Lives</TableCell>
+                  <TableCell align="right">Bankroll Divisor</TableCell>
+                  <TableCell align="right">Chance of Loss</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.lives}>
+                    <TableCell>{row.lives}</TableCell>
+                    <TableCell align="right">{row.value}</TableCell>
+                    <TableCell align="right">
+                      {((1 - winOdds) ** row.lives * 100).toFixed(3) + '%'}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Stack>
       </ThemeProvider>
     </>
   );
